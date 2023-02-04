d063db08ee9ed78f5c6a39edfc5f3cee44591b20
Sejmou <samo.kolter@gmail.com>
2023-02-04 18:11:58 +0100

    example using large dataset for scatterplot with chartjs auto (laggy lol)

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,44 @@
 import Chart from 'chart.js/auto';
 import { getAquisitionsByYear } from './api';
 
+type TrackData = {
+  name: string;
+  bpm: number;
+  durationMs: number;
+  valence: number;
+  energy: number;
+  danceability: number;
+  acousticness: number;
+  instrumentalness: number;
+  liveness: number;
+  loudness: number;
+  speechiness: number;
+  key: number;
+  mode: number;
+  timeSignature: number;
+};
+
 (async function () {
   const data = await getAquisitionsByYear();
   console.log(data);
+
+  const tracks = await fetch('/tracks.json', {
+    headers: {
+      Accept: 'application/json',
+    },
+  }).then(resp => resp.json() as Promise<TrackData[]>);
+  console.log(tracks);
 
   const canvas = document.getElementsByTagName('canvas')[0];
   console.log(canvas);
 
   new Chart(canvas, {
-    type: 'bar',
+    type: 'scatter',
     data: {
-      labels: data.map(row => row.year),
       datasets: [
         {
-          label: 'Acquisitions by year',
-          data: data.map(row => row.count),
+          label: 'Tracks',
+          data: tracks.map(t => ({ x: t.danceability, y: t.energy })),
         },
       ],
     },
